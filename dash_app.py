cb677e6d9edd08dc1455ace3c14b3f66c31c21e8
aahmed-eng <ameenahmed5628@gmail.com>
2026-07-27 12:43:30 +0100

    Changes to be committed: 	modified:   dash_app.py

--- dash_app.py
+++ dash_app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output, callback
 import plotly.express as px
 import pandas as pd
 
@@ -23,11 +23,30 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
         }
     ),
 
+    html.Div([dcc.Dropdown(
+        options=[{'label': region, 'value': region} for region in df['region'].unique()] + ['all'],
+        value='west',
+        id='region-dropdown'
+    )], style={'width':'25%', 'display':'inline-block'}),
+
     dcc.Graph(
         id='example-graph-2',
         figure=fig
     )
 ])
+
+@callback(
+    Output('example-graph-2', 'figure'),
+    Input('region-dropdown', 'value')
+)
+def update_graph(selected_region):
+    if selected_region == 'all':
+        filtered_df = df
+    else:
+        filtered_df = df[df['region'] == selected_region]
+
+    new_fig = px.line(filtered_df, x='date', y='sales', color='region')
+    return new_fig
 
 if __name__ == '__main__':
     app.run(debug=True)
